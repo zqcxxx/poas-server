a34600ad0b918ed1cb71211528c727bcc63b4951
zqc <zqcxxx123@gmail.com>
2019-05-02 23:58:46 +0800

    '备份'

--- controller/admin.js
+++ controller/admin.js
@@ -1,68 +1,67 @@
-const path = require('path');
-CryptoJS = require("crypto-js");
+const path = require('path')
+CryptoJS = require('crypto-js')
 const jsonwebtoken = require('jsonwebtoken')
 
-const Admin =  require('../data/model/admin');
+const Admin = require('../data/model/admin')
 const secret = 'poas_secret'
 
-
-exports.login = async ( ctx ) => {
-    let postData = ctx.request.body;
-    let postname = postData.username;
-    let postpwd =  postData.password;
-    let querydata = await Admin.findOne({
-        where: {
-            username: postname,
-        },
-        attributes: ['password']
-    })
-    if( querydata ){
-        if(querydata.dataValues.password === postpwd){
-            let payload = {
-                username: postname,
-                admin: true
-            }
-            ctx.body = {
-                status: 0,
-                message: '登陆成功',
-                token: jsonwebtoken.sign(
-                    payload,
-                    secret,
-                    // 设置 token 过期时间
-                    { expiresIn: '4h' }// 60 seconds * 60 minutes = 1 hour
-                ),
-            }
-        }else {
-            ctx.body = {
-                status: 1,
-                message: '密码错误'
-            }
-        }
-    }else {
-        ctx.body = {
-            status: 1,
-            message: '无此用户'
-        }
+exports.login = async (ctx) => {
+  let postData = ctx.request.body
+  let postname = postData.username
+  let postpwd = postData.password
+  let querydata = await Admin.findOne({
+    where: {
+      username: postname
+    },
+    attributes: ['password']
+  })
+  if (querydata) {
+    if (querydata.dataValues.password === postpwd) {
+      let payload = {
+        username: postname,
+        admin: true
+      }
+      ctx.body = {
+        status: 0,
+        message: '登陆成功',
+        token: jsonwebtoken.sign(
+          payload,
+          secret,
+          // 设置 token 过期时间
+          { expiresIn: '4h' }// 60 seconds * 60 minutes = 1 hour
+        )
+      }
+    } else {
+      ctx.body = {
+        status: 1,
+        message: '密码错误'
+      }
     }
+  } else {
+    ctx.body = {
+      status: 1,
+      message: '无此用户'
+    }
+  }
 }
 
 exports.getinfo = async (ctx) => {
-    try {
-        let auth = ctx.request.header.authorization;
-        let token =  auth.split(' ')[1];
-        let decoded = jsonwebtoken.verify(token, secret);
-        let info = await Admin.findOne({
-            where: { username : decoded.username}
-        })
-        ctx.body = {
-            status: 0,
-            message: '获取成功',
-            data: info
-        }
-    } catch (error) {
-        ctx.body = {
-            status: 1,
-            message: '获取失败',
-        }
+  try {
+    let auth = ctx.request.header.authorization
+    let token = auth.split(' ')[1]
+    let decoded = jsonwebtoken.verify(token, secret)
+    let info = await Admin.findOne({
+      where: { username: decoded.username }
+    })
+    ctx.body = {
+      status: 0,
+      message: '获取成功',
+      data: info
     }
+  } catch (error) {
+    ctx.body = {
+      status: 1,
+      message: '获取失败'
+    }
+  }
 }
--- data/test.js
+++ data/test.js
@@ -1,5 +1,9 @@
 const test = '[{"value": "2"}, {"value": "3"}, {"value": "4"}]'
-const b = '["2",  "3", "4"]'
+const b = {
+    "title": "测试",
+    "type": "0",
+    "options": "[{\"value\":\"33\"},{\"value\":\"42\"},{\"value\":\"22\"}]"
+}
 
 const json = JSON.parse(test)
 
